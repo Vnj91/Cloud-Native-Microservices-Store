b5fff42657e7360962071fcbf4aabf1578b36f8e
Vanshaj Srivastava <srivastavavnj91@gmail.com>
2026-03-14 20:47:05 +0530

    spring initialization

---
+++ product-service/src/test/java/com/devopsstore/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devopsstore.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

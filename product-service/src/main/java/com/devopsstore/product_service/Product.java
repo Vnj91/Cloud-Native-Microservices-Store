8fc0138d7c4a93b73eb96ce33e4d3a2443759ad2
Vanshaj Srivastava <srivastavavnj91@gmail.com>
2026-03-15 16:19:21 +0530

    dockerfile

---
+++ product-service/src/main/java/com/devopsstore/product_service/Product.java
@@ -0,0 +1,21 @@
+package com.devopsstore.product_service;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity        // Tells the database this is a table
+@Data          // Lombok: automatically creates getters and setters
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+}

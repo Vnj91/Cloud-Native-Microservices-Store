c192d0128f29bfa527952ce9cf4f279bcbc1427b
Vanshaj Srivastava <srivastavavnj91@gmail.com>
2026-03-17 20:26:26 +0530

    user-service commit

---
+++ services/user-service/src/main/java/com/devopsstore/user_service/User.java
@@ -0,0 +1,43 @@
+package com.devopsstore.user_service;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+}
+package com.devopsstore.user_service;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users") // 'user' is often a reserved keyword in Postgres
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+}
